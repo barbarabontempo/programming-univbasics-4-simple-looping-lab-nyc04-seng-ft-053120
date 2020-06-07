ec7241590718d282961c05c7f2213835e63322e2
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-07 12:52:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
-# Write your methods here
+
+# Write your methods heredef loop_message_five_times(message)
+def loop_message_five_times(message)
+  counter = 0
+  while counter < 5
+    puts message
+      counter +=1
+  end
+end
+
+def loop_message_n_times(message, number)
+  counter = 0
+  while counter < number do
+    puts message
+    counter +=1
+  end
+end
